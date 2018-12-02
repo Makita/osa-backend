f707064b48fccca838838d2c9b56cff086c9d607
Makita <justin.choi.000@gmail.com>
2018-12-02 10:46:57 -0700

    Fancy styling and end_time generation moved here

--- src/models/appointment.js
+++ src/models/appointment.js
@@ -1,5 +1,15 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('database.sqlite3');
+const startOfDay = require('date-fns/start_of_day');
+const endOfDay = require('date-fns/end_of_day');
+const areRangesOverlapping = require('date-fns/are_ranges_overlapping');
+
+const SERVICES = require('../resources/services');
+
+const blue = "\x1b[1;36m";
+const red = "\x1b[1;31m";
+const rust = "\x1b[2;31m";
+const reset = "\x1b[0m";
 
 module.exports = class Appointment {
   /**
@@ -9,7 +19,6 @@ module.exports = class Appointment {
    * @param {string} phoneNumber Client's phone number in the format XXX-XXX-XXXX.
    * @param {string} services Services with underscores, separated by commas.
    * @param {string} startTime ISO 8601 representation of when this appointment starts.
-   * @param {string} endTime ISO 8601 representation of when this appointment ends.
    */
   constructor(firstName, lastName, phoneNumber, services, startTime, endTime) {
     this.first_name   = firstName;
@@ -17,7 +26,6 @@ module.exports = class Appointment {
     this.phone_number = phoneNumber;
     this.services     = services;
     this.start_time   = startTime;
-    this.end_time     = endTime;
   }
 
   /**
@@ -32,6 +40,7 @@ module.exports = class Appointment {
       start_time TEXT NOT NULL,
       end_time TEXT NOT NULL
     )`);
+    db.run(`CREATE INDEX IF NOT EXISTS start_time_idx ON appointments(start_time);`);
   }
 
   /**
@@ -40,7 +49,7 @@ module.exports = class Appointment {
    */
   static all(callback) {
     db.all("SELECT first_name, last_name, phone_number, services, start_time, end_time FROM appointments;", (err, rows) => {
-      if(err) console.log("Error:", err);
+      if (err) console.log("Error:", err);
       else console.log("Appointment.all() call successful.");
       callback(rows);
     });
@@ -55,8 +64,30 @@ module.exports = class Appointment {
     FROM appointments
     WHERE start_time >= date('now', 'localtime', '+1 day');`,
     (err, rows) => {
-      if(err) console.log("Error:", err);
+      if (err) console.log("Error:", err);
       else console.log("Appointment.allAfterToday() call successful.");
+      callback(rows);
+    });
+  }
+
+  /**
+   * Gets all the appointments but only for a specific day.
+   * @param {string} date An ISO 8601-compliant time string.
+   * @param {(row) => void} callback A callback function just to use for when the query finishes.
+   */
+  static onDate(date, callback) {
+    db.all(`SELECT id, first_name, last_name, start_time, end_time
+    FROM appointments
+    WHERE DATE(start_time) = DATE(?);`,
+    [date],
+    (err, rows) => {
+      if (err) {
+        console.log("Error:", err);
+      } else {
+        console.log(`> ${blue}Appointment${reset}.${rust}onDate(${red}"${date}"${reset}${rust})
+${red}SELECT ${blue}id${red}, ${blue}first_name${red}, ${blue}last_name${red}, ${blue}start_time${red}, ${blue}end_time${red} FROM ${blue}appointments ${red}WHERE DATE(${blue}start_time${red}) = DATE(${blue}"${date}"${red});
+${reset}Found ${rows.length} rows.`);
+      }
       callback(rows);
     });
   }
@@ -70,7 +101,19 @@ module.exports = class Appointment {
     this.validates_existence_of("phone_number");
     this.validates_existence_of("services");
     this.validates_existence_of("start_time");
-    this.validates_existence_of("end_time");
+
+    Appointment.onDate(new Date(this.start_time), (rows) => {
+      for (let i = 0; i < rows.length; i++) {
+        if (areRangesOverlapping(
+          new Date(this.start_time),
+          new Date(this.end_time),
+          rows[i].start_time,
+          rows[i].end_time
+        )) {
+          throw "Time slot already in use.";
+        }
+      }
+    })
   }
 
   /**
@@ -89,19 +132,34 @@ module.exports = class Appointment {
    * @param {string} callback A callback that's basically just to say that stuff happened correctly.
    */
   create(res, callback) {
+    const services = this.services.split(/,\s*/gu).replace(/\s/gu, '_');
+    this.end_time = this.start_time;
+
+    for (let i = 0; i < SERVICES.length; i++) {
+      for (let a = 0; a < services.length; a++) {
+        if (services[a] === SERVICES[i].slug) {
+          this.end_time = addMinutes(this.end_time, SERVICES[i].time);
+        }
+      }
+    }
+    
     this.validations();
     console.log("Validations all passed.")
+
+    const that = this;
 
     db.run(
       "INSERT INTO appointments (first_name, last_name, phone_number, services, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?)",
       [this.first_name, this.last_name, this.phone_number, this.services, this.start_time, this.end_time],
       function(err) {
-        if(err) {
+        if (err) {
           console.log("Error:", err);
           res.status(500).send(err);
         } else {
-          console.log(this.changes + " rows were added.");
-          callback(this.changes);
+          console.log(`> ${blue}Appointment${reset}.${rust}create()${reset}
+${red}INSERT INTO appointments (${blue}first_name${red}, ${blue}last_name${red}, ${blue}start_time${red}, ${blue}end_time${red}) VALUES (${blue}"${that.first_name}"${red}, ${blue}"${that.last_name}"${red}, ${blue}"${that.phone_number}"${red}, ${blue}"${that.services}"${red}, ${blue}"${that.start_time}"${red}, ${blue}"${that.end_time}"${red});
+${reset}Row successfully added.`);
+          callback();
         }
       }
     );
--- src/server.js
+++ src/server.js
@@ -9,8 +9,10 @@ const Appointment = require('./models/appointment');
 app.use(bodyParser.urlencoded({ extended: true }));
 Appointment.make_db();
 
-app.get('/appointments/:date', (req, res) => {
-  Appointment.onDate(date, (rows) => {
+// app.get('/display/:year/:month/:day', (req, res) => {});
+
+app.get('/appointment/:date', (req, res) => {
+  Appointment.onDate(req.params["date"], (rows) => {
     res.status(200).send(rows);
   });
 });
@@ -24,13 +26,16 @@ app.post('/appointment', (req, res) => {
     req.body.last_name,
     req.body.phone_number,
     req.body.services,
-    req.body.start_time,
-    req.body.end_time
+    req.body.start_time
   );
 
-  appointment.create(res, (row) => {
+  appointment.create(res, () => {
     res.status(200).send(req.body);
   });
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send("WORKS!");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
